f5dced7f73c1c15d3b573e89ec7488091d48ac5d
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2021-08-02 14:53:39 +0900

    8/2

--- demo/src/main/java/com/day/control/SampleController.java
+++ demo/src/main/java/com/day/control/SampleController.java
@@ -1,6 +1,10 @@
 package com.day.control;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +12,10 @@ public class SampleController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "hi";
+	}
+	
+	@PostMapping("/add")
+	public String add(@RequestBody Map<String, String> map) {
+		return map.get("id");
 	}
 }
---
+++ demo/src/test/java/com/day/control/SampleControllerTest.java
@@ -0,0 +1,59 @@
+package com.day.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.ContentResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(SampleController.class)
+class SampleControllerTest {
+	@Autowired
+	//mocker : 가짜객체 - 컨트롤러를 제어해줄 수 있는 dispatcher servlet용 가짜객체 
+	// ~ 가짜 객체에 해당 dispatcher servlet이 주입된것
+	private MockMvc mockMvc;
+	@Test
+	void test() throws Exception{
+		MockHttpServletRequestBuilder requestBuilder;
+		requestBuilder = MockMvcRequestBuilders.get("/hello");
+		requestBuilder.accept(MediaType.APPLICATION_JSON); //요청 형식 지정
+		ResultActions resultActions = mockMvc.perform(requestBuilder);
+		
+		ResultMatcher matcher = MockMvcResultMatchers.status().isOk(); //예상되는 응답상태 코드값 : OK(200)
+		resultActions.andExpect(matcher); //테스트 코드
+		ContentResultMatchers contentMathers = MockMvcResultMatchers.content();
+		String expectedContent="hi";
+		ResultMatcher matcher1 = contentMathers.string(expectedContent);
+		resultActions.andExpect(matcher1);
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() throws Exception{
+		MockHttpServletRequestBuilder requestBuilder;
+		requestBuilder = MockMvcRequestBuilders.post("/add");
+		requestBuilder.accept(MediaType.APPLICATION_JSON); //요청 형식 지정
+		requestBuilder.content("{\"id\":\"id1\"}");
+		ResultActions resultActions = mockMvc.perform(requestBuilder);
+		
+		ResultMatcher matcher = MockMvcResultMatchers.status().isOk(); //예상되는 응답상태 코드값 : OK(200)
+		resultActions.andExpect(matcher); //테스트 코드
+		ContentResultMatchers contentMathers = MockMvcResultMatchers.content();
+		String expectedContent="id1";
+		ResultMatcher matcher1 = contentMathers.string(expectedContent);
+		resultActions.andExpect(matcher1);
+	}
+}
